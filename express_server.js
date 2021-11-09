5eeea07e8ed532bdc29ea2127356f846c519dac0
dahir yusuf <dahir.yusuf@hotmail.com>
2021-11-09 23:07:45 +0000

    Add redirect to long url

--- express_server.js
+++ express_server.js
@@ -19,11 +19,17 @@ const urlDatabase = {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+}); 
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;  
+  res.redirect(`/urls/${shortURL}`)
 });
 
 app.get("/urls", (req, res) => {
